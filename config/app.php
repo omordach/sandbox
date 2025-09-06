429f47e5017e1a173ea0b6a3f244ba6485b78554
Oleh Mordach <omordach@gmail.com>
2025-09-06 19:10:25 +0200

    fixes

--- config/app.php
+++ config/app.php
@@ -1,24 +1,66 @@
 <?php
 
 return [
+
     'name' => env('APP_NAME', 'Laravel'),
+
     'env' => env('APP_ENV', 'production'),
+
     'debug' => (bool) env('APP_DEBUG', false),
+
     'url' => env('APP_URL', 'http://localhost'),
+
     'timezone' => env('APP_TIMEZONE', 'UTC'),
+
     'locale' => env('APP_LOCALE', 'en'),
+
     'fallback_locale' => env('APP_FALLBACK_LOCALE', 'en'),
+
     'cipher' => 'AES-256-CBC',
+
     'providers' => [
-        Illuminate\Foundation\Providers\FoundationServiceProvider::class,
+
+        /*
+        |--------------------------------------------------------------------------
+        | Laravel Framework Service Providers
+        |--------------------------------------------------------------------------
+        */
+
+        Illuminate\Auth\AuthServiceProvider::class,
+        Illuminate\Broadcasting\BroadcastServiceProvider::class,
+        Illuminate\Bus\BusServiceProvider::class,
+        Illuminate\Cache\CacheServiceProvider::class,
         Illuminate\Foundation\Providers\ConsoleSupportServiceProvider::class,
-        Illuminate\View\ViewServiceProvider::class,
-        Illuminate\Filesystem\FilesystemServiceProvider::class,
+        Illuminate\Cookie\CookieServiceProvider::class,
         Illuminate\Database\DatabaseServiceProvider::class,
         Illuminate\Encryption\EncryptionServiceProvider::class,
-        Illuminate\Cookie\CookieServiceProvider::class,
+        Illuminate\Filesystem\FilesystemServiceProvider::class,
+        Illuminate\Foundation\Providers\FoundationServiceProvider::class,
+        Illuminate\Hashing\HashServiceProvider::class,
+        Illuminate\Mail\MailServiceProvider::class,
+        Illuminate\Notifications\NotificationServiceProvider::class,
+        Illuminate\Pagination\PaginationServiceProvider::class,
+        Illuminate\Pipeline\PipelineServiceProvider::class,
+        Illuminate\Queue\QueueServiceProvider::class,
+        Illuminate\Redis\RedisServiceProvider::class,
+        Illuminate\Auth\Passwords\PasswordResetServiceProvider::class,
         Illuminate\Session\SessionServiceProvider::class,
+        Illuminate\Translation\TranslationServiceProvider::class,
+        Illuminate\Validation\ValidationServiceProvider::class,
+        Illuminate\View\ViewServiceProvider::class,
+        Illuminate\Routing\RoutingServiceProvider::class,
+
+        /*
+        |--------------------------------------------------------------------------
+        | Application Service Providers
+        |--------------------------------------------------------------------------
+        */
+
         App\Providers\AppServiceProvider::class,
-        App\Providers\Filament\AdminPanelProvider::class,
+
+        // Uncomment only if Filament is installed
+        // App\Providers\Filament\AdminPanelProvider::class,
     ],
+
 ];
+
