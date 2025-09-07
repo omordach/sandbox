545660528b40e3e77558960f5be2f33b3fa9ac08
Oleh Mordach <omordach@gmail.com>
2025-09-07 11:12:33 +0200

    chore: add wayfinder alias

--- resources/js/actions/Illuminate/Routing/RedirectController.ts
+++ resources/js/actions/Illuminate/Routing/RedirectController.ts
@@ -1,4 +1,4 @@
-import { queryParams, type RouteQueryOptions, type RouteDefinition, type RouteFormDefinition } from './../../../wayfinder'
+import { queryParams, type RouteQueryOptions, type RouteDefinition, type RouteFormDefinition } from '@/wayfinder'
 /**
 * @see \Illuminate\Routing\RedirectController::__invoke
 * @see vendor/laravel/framework/src/Illuminate/Routing/RedirectController.php:19
