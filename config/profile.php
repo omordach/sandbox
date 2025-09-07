e774249775c9dfa99114317c742cd092069f97f4
oleh-mordach <oleh@get-code.net>
2025-09-07 17:04:38 +0200

    feat(config): add profile config for home page

---
+++ config/profile.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'name' => 'Oleh Mordach',
+    'role' => 'Project • Product • Program Management | PMP • PSM • PSPO',
+    'location' => 'Warsaw, PL',
+    'summary' => 'Driving scalable products from vision to delivery. Agile, Cloud, DevOps.',
+    'links' => [
+        ['label' => 'LinkedIn', 'url' => 'https://www.linkedin.com/in/oleh-mordach/'],
+        ['label' => 'GitHub', 'url' => 'https://github.com/'],
+    ],
+];
+
