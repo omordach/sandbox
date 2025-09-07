d387445c582f61a5f0d6eec7d73c6cc2d185e1e4
Oleh Mordach <omordach@gmail.com>
2025-09-07 12:23:31 +0200

    fix: ensure newline at EOF

--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,3 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'welcome');
 Route::get('/ping', fn () => 'pong');
 Route::view('/dashboard', 'welcome')->middleware('auth')->name('dashboard');
-
